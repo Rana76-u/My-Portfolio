35bfbf563167442e78adc31c9d80372f9d7861a6
Rana76-u <62068304+Rana76-u@users.noreply.github.com>
2024-08-25 06:57:28 +0600

    built v2 ui only

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2917840343"
+    serviceWorkerVersion: "3485210853"
   }
 });
